d14172362fd1b9c16ac1d2bf9ec85cb5305fd6f4
Akinmyde <akinremiolumide96@gmail.com>
2021-06-08 08:14:19 +0100

    update package

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import jwt from 'jsonwebtoken';
 
 const baseUrl = 'https://api.magicthegathering.io/v1';
 export const getCards = async (setName = '', pageSize = 10) => {
@@ -14,7 +13,6 @@ export const getCards = async (setName = '', pageSize = 10) => {
 };
 
 export const getSets = async () => {
-  jwt.decode('jfjkfk99095090940905h49504hj');
   const response = await axios.get(`${baseUrl}/sets`);
   if (response.status === 200) {
     const { data } = response;
